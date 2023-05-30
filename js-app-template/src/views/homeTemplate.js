5a88855409ac2b0a518354fe618aaba161da32ed
Kardam Simeonov <masutayodagm@gmail.com>
2023-05-30 13:50:48 +0300

    hero heading

--- js-app-template/src/views/homeTemplate.js
+++ js-app-template/src/views/homeTemplate.js
@@ -2,9 +2,18 @@ import { html } from 'lit-html';
 import layoutTemplate from '../views/layoutTemplate.js';
 
 const homeTemplate = () => layoutTemplate(html`
-  <main class="bg-red-400">
-
-  </main>
+  <div class="relative">
+    <div class="absolute z-10 text-white h-[34rem] w-[55%] flex flex-col justify-center">
+      <div class="absolute bg-red-500 h-full w-[80%] w-96 left-0 top-0"></div>
+      <div class="absolute bg-red-500 h-full w-96 right-0 top-0 transform skew-x-[-20deg]"></div>
+      <div class="pl-12 relative">
+        <h1 class="text-5xl mb-2 drop-shadow-lg">Shop Local, Support Local</h1>
+        <h2 class="text-lg mb-6 drop-shadow-lg">Order food directly from local suppliers</h2>
+        <input type="text" name="search" id="search" class="block p-4 rounded-md shadow-sm text-black w-2/3 border-gray-300 focus:ring-indigo-500 focus:border-indigo-200 sm:text-sm" placeholder="Enter your location...">
+      </div>
+    </div>
+    <img class="relative -right-24 z-0 ml-auto h-[34rem]" src="../src/assets/hero_banner.jpg">
+</div>
 `);
 
 export default homeTemplate;
--- js-app-template/src/views/layoutTemplate.js
+++ js-app-template/src/views/layoutTemplate.js
@@ -1,11 +1,13 @@
 import { html } from 'lit-html';
+import 'iconify-icon';
 
 const layoutTemplate = (content) => html`
-  <header class="shadow-lg px-16 py-4">
+  <header class="shadow-lg px-16 py-2">
     <nav class="flex justify-between">
-      <img class="w-16" src="./src/assets/Food2U_logo.png" alt="Food2U Logo">
-      <div class="flex items-center">
+      <img class="w-14" src="./src/assets/Food2U_logo.png" alt="Food2U Logo">
+      <div class="flex items-center gap-2">
         <span class="font-semibold">Become a supplier</span>
+        <iconify-icon icon="material-symbols:shopping-cart" class="text-2xl"></iconify-icon>
       </div>
     </nav>
   </header>
